918d89f756c4c4409c91ee8d998aede392bbfcfd
Shawn <shawn.chain@gmail.com>
2026-05-19 17:06:16 +0800

    Fix version.cpp generate script work with no .git folder

--- SoapySX/version.sh
+++ SoapySX/version.sh
@@ -4,19 +4,26 @@
 # $2 is the repository root.
 GIT="$1"
 REPO_DIR="${2:-..}"
+TAG_FALLBACK="${3:-unknown}"
+COMMIT_FALLBACK="${4:-unknown}"
 
 # For some reason it sometimes shows dirty if changes have just been committed
 # but stops doing it after running git status.
 # This may not be the right way to fix it but it seems to help.
-"$GIT" -C "$REPO_DIR" status > /dev/null
+if [ -d "$REPO_DIR/.git" ] || [ -f "$REPO_DIR/.git" ]; then
+    "$GIT" -C "$REPO_DIR" status > /dev/null
 
-set -e
+    set -e
 
-printf '%s' 'const char *SoapySX_tag = "'
-"$GIT" -C "$REPO_DIR" describe --tags --always --dirty --broken | tr -d '\n'
-printf '%s\n' '";'
+    printf '%s' 'const char *SoapySX_tag = "'
+    "$GIT" -C "$REPO_DIR" describe --tags --always --dirty --broken | tr -d '\n'
+    printf '%s\n' '";'
 
-printf '%s' 'const char *SoapySX_commit = "'
-"$GIT" -C "$REPO_DIR" rev-parse HEAD | tr -d '\n'
-"$GIT" -C "$REPO_DIR" diff-index --quiet HEAD -- || printf '%s' '-dirty'
-printf '%s\n' '";'
+    printf '%s' 'const char *SoapySX_commit = "'
+    "$GIT" -C "$REPO_DIR" rev-parse HEAD | tr -d '\n'
+    "$GIT" -C "$REPO_DIR" diff-index --quiet HEAD -- || printf '%s' '-dirty'
+    printf '%s\n' '";'
+else
+    printf 'const char *SoapySX_tag = "%s";\n' "$TAG_FALLBACK"
+    printf 'const char *SoapySX_commit = "%s";\n' "$COMMIT_FALLBACK"
+fi
